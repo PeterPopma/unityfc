99c5e95c695c13d81ee6405606ac5bdba1afeedf
Peter Popma <p_popma@hotmail.com>
2022-07-17 10:07:05 +0200

    code for tutorial part 2

--- Crowd.cs
+++ Crowd.cs
@@ -8,11 +8,16 @@ public class Crowd : MonoBehaviour
     [SerializeField] private GameObject pfSpectator2;
     [SerializeField] private GameObject pfSpectator3;
     [SerializeField] private GameObject pfSpectator4;
-    System.Random rnd = new();
+    [SerializeField] private GameObject pfSpectator5;
+    [SerializeField] private GameObject pfSpectator6;
+    [SerializeField] private GameObject pfSpectator7;
+    [SerializeField] private GameObject pfSpectator8;
+    [SerializeField] private float stadiumFillRatio = 0.7f;
+    readonly System.Random rnd = new();
 
     private GameObject SelectRandomSpectator()
     {
-        int value = rnd.Next(4);
+        int value = rnd.Next(8);
         switch(value)
         {
             case 0:
@@ -23,6 +28,14 @@ public class Crowd : MonoBehaviour
                 return pfSpectator3;
             case 3:
                 return pfSpectator4;
+            case 4:
+                return pfSpectator5;
+            case 5:
+                return pfSpectator6;
+            case 6:
+                return pfSpectator7;
+            case 7:
+                return pfSpectator8;
 
             default: return null;
         }
@@ -33,7 +46,6 @@ public class Crowd : MonoBehaviour
     {
         GameObject crowdRoot = GameObject.Find("Crowd");
         var crowdList = new List<GameObject>();
-        var stadium_fill_ratio = 0.6;
         for (int stand = 0; stand <4; stand++)
         {
          /* 
@@ -52,23 +64,23 @@ public class Crowd : MonoBehaviour
             {
                 for (float x = -36.6f; x <= -20.9599991; x += 1.09f)
                 {
-                    if (Random.value < stadium_fill_ratio)
+                    if (Random.value < stadiumFillRatio)
                     {
                         float random_x_offset = (float)((Random.value - 0.5) * 0.6f);
                         GameObject newPerson = Instantiate(SelectRandomSpectator(), new Vector3((float)(random_x_offset + x + 19.23 * stand), y, z), Quaternion.identity);
                         newPerson.transform.Rotate(new Vector3(277.846069f, 180f, 23.5500908f));
-                        //          newPerson.isStatic = true;
+                        newPerson.isStatic = true;
                         newPerson.transform.parent = crowdRoot.transform;
                         crowdList.Add(newPerson);
                     }
 
                     // Mirror the z-axis for the other side
-                    if (Random.value < stadium_fill_ratio)
+                    if (Random.value < stadiumFillRatio)
                     {
                         float random_x_offset = (float)((Random.value - 0.5) * 0.6f);
                         GameObject newPerson = Instantiate(SelectRandomSpectator(), new Vector3((float)(random_x_offset + 0.7f + x + 19.23 * stand), y, -z + 0.5f), Quaternion.identity);
                         newPerson.transform.Rotate(new Vector3(277.846069f, 0f, 23.5500908f));
-                        //          newPerson.isStatic = true;
+                        newPerson.isStatic = true;
                         newPerson.transform.parent = crowdRoot.transform;
                         crowdList.Add(newPerson);
                     }
@@ -95,23 +107,23 @@ public class Crowd : MonoBehaviour
             {
                 for (float z = -3.6f; z >= -23.16; z -= 1.05f)
                 {
-                    if (Random.value < stadium_fill_ratio)
+                    if (Random.value < stadiumFillRatio)
                     {
                         float random_x_offset = (float)((Random.value - 0.5) * 0.6f);
                         GameObject newPerson = Instantiate(SelectRandomSpectator(), new Vector3(x, y, (float)(random_x_offset + z + 25.03 * stand)), Quaternion.identity);
                         newPerson.transform.Rotate(new Vector3(277.846069f, 90f, 23.5500908f));
-                        //          newPerson.isStatic = true;
+                        newPerson.isStatic = true;
                         newPerson.transform.parent = crowdRoot.transform;
                         crowdList.Add(newPerson);
                     }
 
                     // Mirror the x-axis for the other side
-                    if (Random.value < stadium_fill_ratio)
+                    if (Random.value < stadiumFillRatio)
                     {
                         float random_x_offset = (float)((Random.value - 0.5) * 0.6f);
                         GameObject newPerson = Instantiate(SelectRandomSpectator(), new Vector3(-x + 0.7f, y, (float)(random_x_offset + z + 1.2f + 25.03 * stand)), Quaternion.identity);
                         newPerson.transform.Rotate(new Vector3(277.846069f, 270f, 23.5500908f));
-                        //          newPerson.isStatic = true;
+                        newPerson.isStatic = true;
                         newPerson.transform.parent = crowdRoot.transform;
                         crowdList.Add(newPerson);
                     }
@@ -121,13 +133,7 @@ public class Crowd : MonoBehaviour
         }
 
         Debug.Log(crowdList.Count);
-   //     GameObject[] gos = crowdList.ToArray();
-    //    StaticBatchingUtility.Combine(gos, crowdRoot);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject[] gos = crowdList.ToArray();
+        StaticBatchingUtility.Combine(gos, crowdRoot);
     }
 }
